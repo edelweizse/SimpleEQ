ca15f35fcdf87fb728325d7e8f8f7dbafa6f3533
pepechill-1868 <mrmilkor@protonmail.com>
2023-10-14 20:44:24 +0600

    refactored peak coefficient generation

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -38,6 +38,10 @@ enum ChainPositions
 
 };
 
+using Coefficients = Filter::CoefficientsPtr;
+void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+Coefficients makePeakFilter(const ChainSettings& chainSettings, double sampleRate);
+
 //==============================================================================
 /**
 */
@@ -92,8 +96,7 @@ private:
     MonoChain leftChain, rightChain;
 
     void updatePeakFilter(const ChainSettings& chainSettings);
-    using Coefficients = Filter::CoefficientsPtr;
-    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+    
 
     template <int Index, typename ChainType, typename CoefficientsType>
     void update(ChainType& chain, const CoefficientsType& coefficients)
