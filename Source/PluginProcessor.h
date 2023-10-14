723b3678ca905c6091126ab079ad57a2925430e3
pepechill-1868 <mrmilkor@protonmail.com>
2023-10-14 18:34:22 +0600

    gave editor monochain

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -25,6 +25,19 @@ struct ChainSettings
     Slope lowCutSlope { Slope::Slope_12 }, highCutSlope { Slope::Slope_12 };
 };
 ChainSettings getChainSettings(juce::AudioProcessorValueTreeState& apvts);
+
+using Filter = juce::dsp::IIR::Filter<float>;
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+
+};
+
 //==============================================================================
 /**
 */
@@ -76,18 +89,7 @@ public:
     juce::AudioProcessorValueTreeState apvts{ *this, nullptr, "Parameters", createParameterLayout()};
 
 private:
-    using Filter = juce::dsp::IIR::Filter<float>;
-    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
-    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
     MonoChain leftChain, rightChain;
-
-    enum ChainPositions
-    {
-	    LowCut,
-        Peak,
-        HighCut
-        
-    };
 
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
